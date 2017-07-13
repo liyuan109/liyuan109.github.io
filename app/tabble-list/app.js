0ce64ed43386395b1183027af1703940e1c16bde
liyuan109 <lyuan109@gmail.com>
2017-07-12 23:37:15 -0700

    SSO

--- app/tabble-list/app.js
+++ app/tabble-list/app.js
@@ -15,7 +15,10 @@ function TabbleListController($scope) {
       name: 'GitHub',
     },
     {
-      name: 'Current Project'
+      name: 'Current Project',
+    },
+    {
+      name: 'SSO'
     }
   ];
 });
@@ -32,17 +35,18 @@ blogApp.controller('Simple', ['$rootScope', function ($rootScope) {
       }
       else if(tabName == 'Occupation'){
         $rootScope.tabName = 'Occupation'
-        $rootScope.info = 'BIOVIA Corp: Software Test Engineer 2015 - present'
+        $rootScope.info = 'BIOVIA Corp: Software Test Engineer 2015 - 2017' +
+                          'BIOVIA Corp: Software Engineer 2017 - present.'
       }
       else if(tabName == 'Summary'){
         $rootScope.tabName = 'Summary'
-        $rootScope.info = 'Currently write codes to automate tests ' +
+        $rootScope.info = 'Currently work on security team of BIOVIA'/*'Currently write code to automate tests ' +
         'for web applications by using Geb and Spock' + ' Also, used Http builder'
         + ' a very handy Restful client to test API of web applications ' +
         'In addition, all of these are running in continuous integration' +
         ' environment in order to automate the entire process and to achieve regression tests. '+
         'Familiar with agile software development and alway very enthusiastic of ' +
-        ' solving problems'
+        ' solving problems'*/
       }
       else if(tabName == 'GitHub'){
         $rootScope.tabName = 'GitHub'
@@ -51,6 +55,16 @@ blogApp.controller('Simple', ['$rootScope', function ($rootScope) {
       else if(tabName == 'Current Project'){
         $rootScope.tabName = 'Current Project'
         $rootScope.info = 'https://liyuan109.github.io/angularReviewApp'
+      }
+      else if(tabName == 'SSO'){
+        $rootScope.tabName = 'SSO'
+        $rootScope.info = 'SSO handshake seems to be a tricky problem. I usually' +
+                          'For java implementation, there are trust store and key store' +
+                          'These two things are both derived from java keystore class' +
+                          'Usually client needs to trust server and server has to provide its' +
+                          'public key to client and has to be stored in the trust store of client' +
+                          'If mutual authentication needs to be set up, then the server has to trust' +
+                          'the client as well'
       }
     }
 }]);
