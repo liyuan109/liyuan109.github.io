ad0c05e5f7398405da33f4c497c3ea046ebda84a
nate <nate>
2020-12-16 23:26:59 -0800

    add data layer

--- app/tabble-list/app.js
+++ app/tabble-list/app.js
@@ -22,6 +22,15 @@ function TabbleListController($scope) {
     }
   ];
 });
+var digitalData = {
+  test: "value",
+  cart: {
+    cartID: "123",
+    price: {
+      cartTotal: "18"
+    }
+  }
+}
 blogApp.controller('Simple', ['$rootScope', function ($rootScope) {
     $rootScope.tabName = 'Personal Info'
     $rootScope.info = 'My name is Yuan Li.' + ' People Call me Nate.' +
